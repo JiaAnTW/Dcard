6954621756a32a7048d5ef55c3bcc64591298732
JiaAnTW <at5lp6andy@gmail.com>
2021-05-03 00:36:26 +0800

    feat: improve useScrollBottomEffect

--- src/utils/useScrollBottomEffect.js
+++ src/utils/useScrollBottomEffect.js
@@ -1,28 +1,16 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useEffect } from 'react';
+import useScroll from './useScroll';
 
 function useScrollBottomEffect(onScrollBottom, ref) {
-    const [scrollTop, setScrollTop] = useState(0);
-
-    const handleOnScroll = useCallback((e) => {
-        setScrollTop(e.target.scrollTop);
-    }, []);
-
-    useEffect(() => {
-        if (ref.current) {
-            ref.current.addEventListener('scroll', handleOnScroll);
-        }
-
-        return () => {
-            ref.current.removeEventListener('scroll', handleOnScroll);
-        };
-    }, [ref]);
+    const scrollTop = useScroll(ref);
 
     useEffect(() => {
         if (
             ref.current &&
-            ref.current.scrollTop !== 0 && // 這行是為了避免初始沒有scroll時觸發effect
-            scrollTop >=
-                ref.current.scrollHeight - ref.current.offsetHeight - 10
+            // 這行是為了避免初始沒有scroll時觸發effect
+            ref.current.scrollTop !== 0 &&
+            // 實際觀察發現會有0.2左右的誤差，所以需要減1
+            scrollTop >= ref.current.scrollHeight - ref.current.offsetHeight - 1
         ) {
             onScrollBottom();
         }
