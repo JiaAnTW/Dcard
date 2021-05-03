e9f444dad2836188d4e96d53e6038ca9b03920a4
JiaAnTW <at5lp6andy@gmail.com>
2021-05-03 13:23:00 +0800

    fix: remove default value of VirtualWindow

--- src/components/VirtualWindow/index.js
+++ src/components/VirtualWindow/index.js
@@ -3,39 +3,37 @@ import React, { useState, useEffect, forwardRef } from 'react';
 import useScroll from '@/utils/useScroll';
 import { InnerContainer } from './style';
 
-const VirtualWindow = forwardRef(
-    ({ Container = <div />, height, children }, ref) => {
-        const [elements, setElements] = useState(undefined);
-        const [topIndex, setTopIndex] = useState(0);
-        const [bottomIndex, setBottomIndex] = useState(0);
-        const scrollTop = useScroll(ref);
+const VirtualWindow = forwardRef(({ Container, height, children }, ref) => {
+    const [elements, setElements] = useState(undefined);
+    const [topIndex, setTopIndex] = useState(0);
+    const [bottomIndex, setBottomIndex] = useState(0);
+    const scrollTop = useScroll(ref);
 
-        useEffect(() => {
-            setTopIndex(Math.floor(scrollTop / height));
-            setBottomIndex(
-                Math.floor((scrollTop + ref.current.offsetHeight) / height) + 2
-            );
-        }, [scrollTop]);
-
-        useEffect(() => {
-            const nextChildren = React.Children.map(children, (item, index) => {
-                if (index < topIndex) return null;
-                if (index > bottomIndex) return null;
-                return React.cloneElement(item, {
-                    style: { position: 'absolute', top: `${index * height}px` },
-                });
-            });
-            setElements(nextChildren);
-        }, [topIndex, bottomIndex, children, ref]);
-
-        return (
-            <Container ref={ref}>
-                <InnerContainer height={children.length * height}>
-                    {elements}
-                </InnerContainer>
-            </Container>
+    useEffect(() => {
+        setTopIndex(Math.floor(scrollTop / height));
+        setBottomIndex(
+            Math.floor((scrollTop + ref.current.offsetHeight) / height) + 2
         );
-    }
-);
+    }, [scrollTop]);
+
+    useEffect(() => {
+        const nextChildren = React.Children.map(children, (item, index) => {
+            if (index < topIndex) return null;
+            if (index > bottomIndex) return null;
+            return React.cloneElement(item, {
+                style: { position: 'absolute', top: `${index * height}px` },
+            });
+        });
+        setElements(nextChildren);
+    }, [topIndex, bottomIndex, children, ref]);
+
+    return (
+        <Container ref={ref}>
+            <InnerContainer height={children.length * height}>
+                {elements}
+            </InnerContainer>
+        </Container>
+    );
+});
 
 export default VirtualWindow;
