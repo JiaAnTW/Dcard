5a1a737c01ced8dc859e3d2dafdcf0259b00572f
JiaAnTW <at5lp6andy@gmail.com>
2021-05-03 12:39:46 +0800

    fix: correct box-size of Card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,8 +2,10 @@ import styled from 'styled-components';
 
 export const Card = styled.div`
     border: 1px solid rgba(0, 0, 0, 0.125);
-    padding: 5px;
+    padding: 15px;
     height: 155px;
+    width: 100%;
+    box-sizing: border-box;
 `;
 
 export const CardTitle = styled.h4``;
